5a2475913a1602098ee1b2a2b324924938b55afc
Vijaya Chandran Mani <vijaycs85@gmail.com>
2019-03-02 21:50:06 +0000

    Adding launcher

---
+++ index.js
@@ -0,0 +1,19 @@
+const lighthouse = require('lighthouse');
+const chromeLauncher = require('chrome-launcher');
+
+function launchChromeAndRunLighthouse(url, opts, config = null) {
+  return chromeLauncher.launch({chromeFlags: opts.chromeFlags}).then(chrome => {
+    opts.port = chrome.port;
+    return lighthouse(url, opts, config).then(results => {
+      return chrome.kill().then(() => results.report)
+    });
+  });
+}
+
+const opts = {
+  chromeFlags: ['--show-paint-rects']
+};
+
+launchChromeAndRunLighthouse('https://bbc.co.uk', opts).then(results => {
+  console.log(results);
+});
